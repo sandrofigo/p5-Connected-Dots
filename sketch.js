efce62a27729dfc2b37efa90543ce3e1f1134d8f
FireFlame74 <sandro.figo@gmx.at>
2018-05-08 20:25:38 +0200

    Changed particle rendering

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 let particles = [];
-let pointCount = 50;
 const particleCount = 150;
 const maxDistance = 50;
 const maxConnections = 3;
 const minSpeed = 0.05;
 const maxSpeed = 0.15;
 
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     background(41, 47, 54);
-    for (let i = 0; i < pointCount; i++) {
+    for (let i = 0; i < particleCount; i++) {
         let p = new Particle();
         particles.push(p);
     }
@@ -19,31 +19,63 @@ function setup() {
 
 function draw() {
     background(41, 47, 54);
-    stroke(242, 147, 24);
+    //orange
+    //const particleColor = color(242, 147, 24);
+    const particleColor = color(127);
+    const lineColor = color(255, 255, 255);
     
+
     for (let i = 0; i < particles.length; i++) {
+        noStroke();
+        fill(particleColor);
         particles[i].draw();
         particles[i].update();
     }
 
-    for (let i = 0; i < particles.length-1; i++) {
-        let d = particles[i].position.dist(particles[i+1].position);
-        stroke(255,255,255, map(d, 0, 100, 255, 0));
-        line(particles[i].position.x, particles[i].position.y, particles[i+1].position.x, particles[i+1].position.y);
+    for (let i = 0; i < particles.length - 1; i++) {
+
+        let current = particles[i];
+        let affected = [];
+
+        for (let j = 0; j < particles.length; j++) {
+            if (particles[j] != current) {
+                let distance = current.position.dist(particles[j].position);
+                if (distance <= maxDistance && affected.length <= maxConnections) {
+                    affected.push({
+                        'particle': particles[j],
+                        'distance': distance
+                    });
+                }
+            }
+        }
+
+        for (let a = 0; a < affected.length; a++) {
+            stroke(red(lineColor), green(lineColor), blue(lineColor), map(affected[a].distance, 0, maxDistance, 255, 0));
+            //smooth();
+            line(current.position.x, current.position.y, affected[a].particle.position.x, affected[a].particle.position.y);
+        }
+
+        
     }
 }
 
 function Particle() {
     this.position = createVector(random(0, window.innerWidth), random(0, window.innerHeight));
-    this.velocity = createVector(random(-1,1), random(-1,1));
+    this.velocity = createVector(random(-1, 1), random(-1, 1));
     this.velocity.normalize();
-    this.velocity.mult(random());
+    this.velocity.mult(random(minSpeed, maxSpeed));
 
-    this.update = function(){
+    this.update = function () {
         this.position.add(this.velocity);
+        if (this.position.x < 0) this.position.x = window.innerWidth;
+        if (this.position.y < 0) this.position.y = window.innerHeight;
+        if (this.position.x > window.innerWidth) this.position.x = 0;
+        if (this.position.y > window.innerHeight) this.position.y = 0;
     };
 
-    this.draw = function(){
-        point(this.position.x, this.position.y);
+    this.draw = function () {
+        //point(this.position.x, this.position.y);
+        
+        ellipse(this.position.x, this.position.y, 2, 2);
     };
 }
